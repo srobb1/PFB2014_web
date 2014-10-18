1d31189e2d0be6aa19b9d39295525c5c4ca884cf
Ubuntu <ubuntu@ip-10-102-151-198.ec2.internal>
2014-10-18 04:52:40 +0000

    add solutions for unix and PerlI-III, update index.html

--- review_sessions/passingRefs.pl
+++ review_sessions/passingRefs.pl
@@ -2,25 +2,35 @@
 use warnings;
 use strict;
 
+# initialize array to store all our data
 my @micro;
 
-foreach my $i (0..5000) {
-    foreach my $j (0..3000) {
-	$micro[$i][$j] = int rand(10);
+#let's make a big rectangle of random numbers
+foreach my $i (0..5000) { # first dimension
+    foreach my $j (0..3000) { #second dimension
+	$micro[$i][$j] = int rand(10); #random number 0-9
     }
 }
-#generate reference
+#generate reference so we can pass a single scalar into the sub
 my $ref = \@micro;
+
 # call function to calculate average
+# reutrns the average
 my $ave = table_average($ref);
+
 print "Average of table = $ave\n";
 
+########################################################
 sub table_average {
-    my $ref = shift;
+    my $ref = shift; # reference to array of arrays
     my $total = 0;
     my $count = 0;
-    foreach my $i_ref (@{$ref}) {
-	foreach my $j ( @{$i_ref} ) {
+    foreach my $i_ref (@{$ref}) { # get each of the refs in the first
+                                  # dimension that point to the arrays
+                                  # in the second dimension
+	foreach my $j ( @{$i_ref} ) { # dereference the second dimension
+                                      # array ref to get the array 
+                                      # with all the values (-> $j)
 	    $total += $j;
 	    $count++;
 	}
